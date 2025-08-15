ada46921f69ad9d6ae5d419c0b87867acc91dd72
Bryan <bryanfonsecafernandes@gmail.com>
2025-08-15 23:43:56 +0000

    Carta 1 e Carta 2 Criados

--- xadrez.c
+++ xadrez.c
@@ -2,4 +2,22 @@
 
 int main() {
 printf ("Desafio de Xadrez!/n");
+char estado1= "A";
+char codigo1[1]= "A01";
+char nomeCidade1 [20] = "Rio de Janeiro";
+int populacao1 = 6000000;
+float area1 = 1200;
+float pib1 = 300.00;
+int pontosTuristicos1 = 26;
+
+char estado2 ="B";
+char codigo2 =[2] = "B02";
+char nomeCidade2 [20] = "Rio Grande do Sul"
+int populacao2 = 10000000;
+float area2 =2200;
+float pib2 = 480.00;
+int pontosTuristicos2 = 30;
+
+
+
 }
