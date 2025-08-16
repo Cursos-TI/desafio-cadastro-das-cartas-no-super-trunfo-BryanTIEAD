5ea852371bc6817cd7254afe64f4ec1619242a8a
Bryan <bryanfonsecafernandes@gmail.com>
2025-08-16 00:21:19 +0000

    Dados inseridos organizadamente

--- xadrez.c
+++ xadrez.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 
 int main() {
-printf ("Desafio de Xadrez!/n");
-char estado1= "A";
-char codigo1[1]= "A01";
-char nomeCidade1 [20] = "Rio de Janeiro";
-int populacao1 = 6000000;
-float area1 = 1200;
-float pib1 = 300.00;
-int pontosTuristicos1 = 26;
-
-char estado2 ="B";
-char codigo2 =[2] = "B02";
-char nomeCidade2 [20] = "Rio Grande do Sul"
-int populacao2 = 10000000;
-float area2 =2200;
-float pib2 = 480.00;
-int pontosTuristicos2 = 30;
-
+    printf ("Desafio Xadrez/n");
+char estado1, estado2; 
+char codigo1[1], codigo2[2];
+char nomeCidade1[20], nomeCidade2[20];
+int populacao1, populacao2;
+float area1, area2;
+float pib1, pib2;
+int pontosTuristicos1, pontosTuristicos2;
 
 
 }
