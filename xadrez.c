05c173834254b2b799b5edd35ceba583f18ef424
Bryan <bryanfonsecafernandes@gmail.com>
2025-08-16 00:57:23 +0000

    Cadastro da carta 1 corretamente

--- xadrez.c
+++ xadrez.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
 
 int main() {
-    printf ("Desafio Xadrez/n");
-char estado1, estado2; 
-char codigo1[1], codigo2[2];
-char nomeCidade1[20], nomeCidade2[20];
-int populacao1, populacao2;
-float area1, area2;
-float pib1, pib2;
-int pontosTuristicos1, pontosTuristicos2;
+    printf ("Desafio Xadrez\n");
+char estado1[A], estado2{B}; 
+char codigo1[A01], codigo2[B02];
+char nomeCidade1[20] ("Ceara"), nomeCidade2[20]; ("Brasilia")
+int populacao1 = 1000000, populacao2 = 500000;
+float area1 = 1200.25, area2 = 1120.50;
+float pib1 = 250000000, pib2 = 500000000;
+int pontosTuristicos1 = 45, pontosTuristicos2 = 15;
 
+printf("Cadastro da Carta1\n");
+printf("Estado: A\n");
+scanf(" %c", &estado1);
+printf("Cadastro da Carta [A01]\n);
+    scanf("%s", codigo1);
+    printf("Nome da cidade:");
+    scanf("%[^\n]", nomeCidade1);
+    printf("Populacao:");
+    scanf("%d"), &populacao1);
+    printf("Area (km²): ")
+    scanf ("%f", &area1");
+    printf("PIB:");
+    scanf("%f", %pib1);
+    printf("Numero de Pontos Turisticos: ")
+    scanf("%d", &pontosTuristicos1);
 
+        
+
+return 0
 }
